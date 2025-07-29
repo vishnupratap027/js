f3a69d608725c6588237d50f7be09f68c867992e
vishnupratap027 <168295597+vishnupratap027@users.noreply.github.com>
2025-07-29 17:03:37 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3)
+//code readeablity should be high
+//ECMAScript
+
+let name="vishnu pratap singh"
+let age="25"
+let isloggedin=true
+
+//number =>2 to power 53
+//bigint
+//string => ""
+//boolean =>true/false
+//null => standalone value
+//undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof(age))
+// null type is object 
+// undefine type is underfined
