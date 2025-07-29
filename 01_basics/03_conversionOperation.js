7e33c248d97a6825a1013d834cb31f4b540131bf
vishnupratap027 <168295597+vishnupratap027@users.noreply.github.com>
2025-07-29 17:48:36 +0000

    conversion_of_datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "goldy"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33" => 33
+// "33abc" => NaN
+// true=>1; false => 0
+
+let somenumber=33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
