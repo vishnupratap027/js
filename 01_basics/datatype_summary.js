42639c3c9ac2a115a043520c7fa24ade763ee0bd
vishnupratap027 <168295597+vishnupratap027@users.noreply.github.com>
2025-07-31 10:08:00 +0000

    datatype_gyan++

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -1,3 +1,34 @@
 // primitive
 
-//7 Types : String, Number, BigInt, Boolean, Undefined, Null, Symbol
+// //7 Types : String, Number, BigInt, Boolean, Undefined, Null, Symbol
+
+const score = 100;
+const scoreValue =100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id =Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id == anotherId);
+
+const bigNumber = 384884484848n;
+
+// Reference (Non primitive)
+
+const heros =["Ironman","Thor","Spiderman"]
+let myobj = {
+    name:"Ramukaka",
+    Age:"55",
+};
+
+const myFuction = function() {
+    console.log("Nameste sahabjiii");
+};
+myFuction();
+console.log(typeof bigNumber)
+
+// Array, Objects, functions
+
