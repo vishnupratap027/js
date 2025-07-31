3bb53f42f339698499e516575593f9e3aebbb6ea
vishnupratap027 <168295597+vishnupratap027@users.noreply.github.com>
2025-07-31 11:57:07 +0000

    nums_and_maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,31 @@
+// const score = '400'
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hunderds = 1000000
+// console.log(hunderds.toLocaleString('en-IN'))
+
+//++++++++++++++++++++++++++++++++++maths+++++++++++++++++++++
+
+console.log(Math)
+console.loge(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9)); 
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.random()*(max-min+1)+min)
